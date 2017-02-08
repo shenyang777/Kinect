fbb73b079e87f4ce47c3a755ff2b54c95bd001eb
shenyang777 <shenyang@iastate.edu>
2017-02-08 17:51:10 -0600

    Add files via upload

---
+++ DepthAndColor/PlaybackClip.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Kinect.Tools;
+
+namespace Microsoft.Samples.Kinect.DepthAndColor
+{
+    class PlaybackHelper
+    {
+        //static int num=0;
+        public static void PlaybackClip(string filePath, uint loopCount,ref int num)
+        {
+            using (KStudioClient client = KStudio.CreateClient())
+            {
+                client.ConnectToService();
+
+                using (KStudioPlayback playback = client.CreatePlayback(filePath))
+                {
+                    playback.LoopCount = loopCount;
+                    
+                    playback.Start();
+
+                    while ((playback.State == KStudioPlaybackState.Playing) || (playback.State == KStudioPlaybackState.Paused))
+                    {
+
+                        
+                        {
+                            if ((playback.State == KStudioPlaybackState.Playing))
+                            {
+                                ///////////////////////////////////////////////////////playback.Pause();
+                            }
+                            if (playback.State == KStudioPlaybackState.Paused)
+                            {
+                                playback.StepOnce();
+                                Thread.Sleep(100);
+                                num = num + 1;
+                            }
+                            
+                        }
+                        
+                        Thread.Sleep(100);
+                    }
+                    playback.Stop();            
+
+
+                    if (playback.State == KStudioPlaybackState.Error)
+                    {
+                        throw new InvalidOperationException("Error: Playback failed!");
+                    }
+                }
+
+                client.DisconnectFromService();
+            }
+        }
+    }
+}
